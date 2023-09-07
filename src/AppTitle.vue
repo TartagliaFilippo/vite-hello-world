68925e9e4a0f9aeaffde460f4c775e7b6cc2d1a6
filippo199600 <pippotartaglia@gmail.com>
2023-09-07 15:34:29 +0200

    first commit

---
+++ src/AppTitle.vue
@@ -0,0 +1,18 @@
+<script>
+import AppImage from "./components/AppImage.vue";
+export default {
+  data() {
+    return {
+      title: "La mia prima app con Vite!",
+    };
+  },
+  components: { AppImage },
+};
+</script>
+
+<template>
+  <h1>{{ title }}</h1>
+  <AppImage />
+</template>
+
+<style></style>
---
+++ src/components/AppImage.vue
@@ -0,0 +1,7 @@
+<script></script>
+
+<template>
+  <img src="../components/img/chomsky.jpg" alt="" />
+</template>
+
+<style></style>
